96e289fb057e58da55510b00f50e545bd26d61c8
xusancai <sancaixu@163.com>
2020-09-11 15:41:26 +0800

    创建启动项目

---
+++ iOSDemo/iOSDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOSDemo
+//
+//  Created by xu sancai on 2020/9/11.
+//  Copyright © 2020 xu sancai. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ViewController : NSViewController
+
+
+@end
+
---
+++ iOSDemo/iOSDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  iOSDemo
+//
+//  Created by xu sancai on 2020/9/11.
+//  Copyright © 2020 xu sancai. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    // Do any additional setup after loading the view.
+}
+
+
+- (void)setRepresentedObject:(id)representedObject {
+    [super setRepresentedObject:representedObject];
+
+    // Update the view, if already loaded.
+}
+
+
+@end
