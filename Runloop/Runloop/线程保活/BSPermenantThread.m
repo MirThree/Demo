c2e87ead94663405fdfb897ddcdce01b5c9d1219
xusancai <sancaixu@163.com>
2020-09-11 17:25:42 +0800

    添加创建runloop的注释

--- Runloop/Runloop/线程保活/BSPermenantThread.m
+++ Runloop/Runloop/线程保活/BSPermenantThread.m
@@ -31,10 +31,19 @@
 -(instancetype)init {
     if (self = [super init]) {
         self.innerThread = [[BSThread alloc] initWithBlock:^{
+            // 创建上下文（要初始化一下结构体）
             CFRunLoopSourceContext content = {0};
+            
+            // 创建source
             CFRunLoopSourceRef source = CFRunLoopSourceCreate(kCFAllocatorDefault, 0, &content);
+            
+            // 往Runloop中添加source
             CFRunLoopAddSource(CFRunLoopGetCurrent(), source, kCFRunLoopDefaultMode);
+            
+            // 销毁source
             CFRelease(source);
+            
+            // 启动
             CFRunLoopRunInMode(kCFRunLoopDefaultMode, 1.0e10, false);
             NSLog(@"end ---- ");
         }];
